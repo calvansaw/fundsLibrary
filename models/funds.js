bfb271746cbd294469a93ee4787cc9d7599d481f
calvansaw <calvansaw@hotmail.com>
2021-02-24 01:32:32 +0800

    created funds and users models

---
+++ models/funds.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+////create schema//////
+const fundSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	riskRating: { type: String, required: true },
+	fundSize: { type: Number, required: true },
+	fundSizeFx: { type: String, uppercase: true, required: true },
+	managementFee: { type: Number, required: true },
+	dividendDistFreq: String,
+	inceptionDate: Date,
+	performanceYtd: { type: Number, required: true },
+	performance1Yr: { type: Number, required: true },
+	performance3Yr: { type: Number, required: true },
+	performance5Yr: { type: Number, required: true },
+	nav: { type: Number, required: true },
+	navFx: { type: String, uppercase: true, required: true },
+	product: { type: String, required: true },
+	assetManager: { type: String, required: true },
+	assetClass: { type: String, required: true },
+	subAssetClass: { type: String, required: true },
+	sharpeRatio1Yr: { type: Number, required: true },
+	sharpeRatio3Yr: { type: Number, require: true },
+	sharpeRatio5Yr: { type: Number, required: true },
+	volatility1Yr: { type: Number, required: true },
+	volatility3Yr: { type: Number, required: true },
+	volatility5Yr: { type: String, required: true },
+	paymentType: [String],
+});
+
+////create model/////
+const Fund = mongoose.model('Fund', fundSchema);
+
+////export/////
+module.exports = Fund;
---
+++ server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const mongoURI = process.env.DB_URI || 'mongodb://localhost:27017/fund';
+
+/////////////// connect to mongoose /////////////
+mongoose.connect(mongoURI, {
+	useNewUrlParser: true,
+});
+mongoose.connection.once('open', () => {
+	console.log('connected to mongo');
+});
+
+////////////////// middleware ///////////////////
+// for put delete routes
+app.use(methodOverride('_method'));
+// body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// static files
+app.use(express.static('public'));
+// session
+app.use(
+	session({
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
+////////////////// controllers ///////////////////
+app.get('/', (req, res) => {
+	res.send('Hello World fundsLibrary');
+});
+
+////// create new user ///////
+const userController = require('./controllers/users');
+app.use('/users', userController);
+
+//////////////////////////////////////////////////
+app.listen(port, () => {
+	console.log('Listening at port', port + ' ' + mongoURI);
+});
